86678c14159f5707079b61b109a0a1968c013615
Abdul Jaweed <jdgaming7320@gmail.com>
2023-09-20 17:52:21 +0530

    adding Day 03

---
+++ Resource/Day 03 Start C++ from Zero and Write Your First Program/First.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+// int main() {
+// //   std::cout<<"Hello Coder Army"<<endl;  // <<endl (Incersion operator)
+// //   cout<<"Hello Coder Army \nHello World \n"; 
+
+// //   cout<<3+5<<endl;
+
+// //   // Variable declare
+
+// //   // Integer
+// //   int a = 20, b = 50, c = 80;
+// //   cout<<a+b+c;
+
+//   // String 
+
+// //   char d = 'a';
+// //   cout<<d;
+
+//     // float f = 5.67;
+//     // cout<<f<<endl;
+
+//     // double d = 5.256984;
+//     // cout<<d<<endl;
+
+//     // boolean
+
+//     bool b = true;
+//     cout<<b;
+// };
+
+
+
+// 1. Two numbers are given, print their product
+
+// int main() {
+//     int a = 2, b = 5;
+//     cout<<a*b<<endl;
+// }
+
+// 2. Two numbers are given a and b, print a-b
+
+// int main() {
+//     int a = 4, b = 2;
+//     cout<<a-b<<endl;
+// }
+
+// 3. Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen.
+
+// int main() {
+//     cout<<"6*3"<<endl;
+// }
+
---
+++ Resource/Day 03 Start C++ from Zero and Write Your First Program/README.md
@@ -0,0 +1,27 @@
+## Coding Assignment
+
+**1. Two numbers are given, print their product**
+
+```cpp
+int main() {
+    int a = 2, b = 5;
+    cout<<a*b<<endl;
+}
+```
+
+**2. Two numbers are given a and b, print a-b**
+
+```cpp
+int main() {
+    int a = 4, b = 2;
+    cout<<a-b<<endl;
+}
+```
+
+**3. Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen.**
+
+```cpp
+int main() {
+    cout<<"6*3"<<endl;
+}
+```
